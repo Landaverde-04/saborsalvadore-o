7aa2e3f5f927fa169c06bad02ee4555b937a5e23
Landaverde-04 <landaverde.kevin04@gmail.com>
2025-10-28 08:25:27 -0600

    para produccion

--- ComidaSalvadoreña/settings.py
+++ ComidaSalvadoreña/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import dj_database_url
 
 from pathlib import Path
 
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -74,25 +76,11 @@ WSGI_APPLICATION = 'ComidaSalvadoreña.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-# 1. ESPECIFICA EL MOTOR (ENGINE)
-        'ENGINE': 'django.db.backends.postgresql', 
-        
-        # 2. NOMBRE DE LA BASE DE DATOS LOCAL
-        'NAME': 'saborsalvadoreno_db',          
-        
-        # 3. USUARIO DE LA BASE DE DATOS
-        'USER': 'prueba',              
-        
-        # 4. CONTRASEÑA DEL USUARIO
-        'PASSWORD': 'prueba',     
-        
-        # 5. HOST (Normalmente localhost en desarrollo)
-        'HOST': 'localhost',                  
-        
-        # 6. PUERTO (El puerto por defecto de PostgreSQL es 5432)
-        'PORT': '5432',
-    }
+'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
 }
 
 
@@ -134,5 +122,14 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+# This production code might break development mode, so we check whether we're in DEBUG mode
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
